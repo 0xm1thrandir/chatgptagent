ea3e70a0d25afa48641db145c278449951b0869f
0xm1thrandir <115853038+0xm1thrandir@users.noreply.github.com>
2023-03-08 13:08:54 +0400

    Update therapist.py

--- therapist.py
+++ therapist.py
@@ -1,24 +1,37 @@
-import gradio as gr
 import openai, config, subprocess
-openai.api_key = config.OPENAI_API_KEY
+import gradio as gr
+from flask import Flask, request
 
+openai.api_key = config.OPENAI_API_KEY
 messages = [{"role": "system", "content": 'You are a therapist. Respond to all input in 25 words or less.'}]
 
-def transcribe(audio):
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+def chat():
     global messages
 
-    audio_file = open(audio, "rb")
+    # Receive audio file
+    file = request.files["audio"]
+
+    # Transcribe audio using OpenAI API
+    audio_file = file.read()
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
 
+    # Add user message to messages list
     messages.append({"role": "user", "content": transcript["text"]})
 
+    # Generate AI response using OpenAI API
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-
     system_message = response["choices"][0]["message"]
+
+    # Add AI response to messages list
     messages.append(system_message)
 
+    # Speak AI response
     subprocess.call(["say", system_message['content']])
 
+    # Generate chat transcript
     chat_transcript = ""
     for message in messages:
         if message['role'] != 'system':
@@ -26,5 +39,10 @@ def transcribe(audio):
 
     return chat_transcript
 
-ui = gr.Interface(fn=transcribe, inputs=gr.Audio(source="microphone", type="filepath"), outputs="text").launch()
-ui.launch()
+# Create a Gradio interface to test the app locally
+interface = gr.Interface(fn=chat, inputs=gr.Audio(source="microphone", type="file"), outputs="text")
+interface.test_launch()
+
+# Uncomment the following line to launch the app in a browser window
+# interface.launch()
+
